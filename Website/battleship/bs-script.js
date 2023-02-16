865228ee999b8497ff8ce88f48fe702d34fdc35e
BetterMJ <M.Jakob.inf21@stud.bbbaden.ch>
2023-02-17 00:16:02 +0100

    BattleShips

--- Website/battleship/bs-script.js
+++ Website/battleship/bs-script.js
@@ -1,6 +1,5 @@
 // create grid
 const leftBoard = document.querySelector("#left-board");
-
 for (let i = 0; i < 100; i++) {
   let cell = document.createElement("div");
   cell.classList.add("grid-cell");
@@ -8,22 +7,290 @@ for (let i = 0; i < 100; i++) {
 }
 
 const rightBoard = document.querySelector("#right-board");
-
 for (let i = 0; i < 100; i++) {
   let cell = document.createElement("div");
   cell.classList.add("grid-cell");
+  cell.addEventListener("click", (e) => {
+    Attacked(e.target);
+  });
   rightBoard.appendChild(cell);
 }
 
-let grid = document.querySelector("#right-board");
-
-let cells = grid.getElementsByClassName("grid-cell");
-
-function clicked(event) {
-  let cell = event.target;
-  cell.style.background = "#FF0000";
-}
-
+let cells = document.querySelectorAll(".grid-cell");
 for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", clicked);
+  cell = cells[i];
+  cell.setAttribute("isOccupied", "0");
+  cell.setAttribute("isHit", "0");
+  cell.style.background = "#6082B6";
 }
+
+// Game functionality
+const leftShipColor = "#818589";
+const missColor = "#36454F";
+const hitColor = "#FF4433";
+const destroyedColor = "#FFBF00";
+
+let leftCells = leftBoard.querySelectorAll(".grid-cell");
+let rightCells = rightBoard.querySelectorAll(".grid-cell");
+let currentTurn = 0;
+
+StartGame();
+
+function StartGame() {
+  PlacePiecesLeft();
+  PlacePiecesRight();
+}
+function PlacePiecesLeft() {
+  let count5 = 0;
+  let count4 = 0;
+  let count3 = 0;
+  let count2 = 0;
+  let has5 = false;
+  let has4 = false;
+  let has3 = false;
+  let has2 = false;
+  let countDone = false;
+  while (!countDone) {
+    let i = getRandom(99);
+    if (!has5) {
+      if (i % 10 <= 5) {
+        if (!isOccupiedLeft(i, 5) && !isAdjacentCellOccupied(i, 5, leftCells)) {
+          PlaceLeft(i, 5);
+          count5++;
+        }
+      }
+    }
+
+    if (!has4) {
+      if (i % 10 <= 6) {
+        if (!isOccupiedLeft(i, 4) && !isAdjacentCellOccupied(i, 4, leftCells)) {
+          PlaceLeft(i, 4);
+          count4++;
+        }
+      }
+    }
+    if (!has3) {
+      if (i % 10 <= 7) {
+        if (!isOccupiedLeft(i, 3) && !isAdjacentCellOccupied(i, 3, leftCells)) {
+          PlaceLeft(i, 3);
+          count3++;
+        }
+      }
+    }
+    if (!has2) {
+      if (i % 10 <= 8) {
+        if (!isOccupiedLeft(i, 2) && !isAdjacentCellOccupied(i, 2, leftCells)) {
+          PlaceLeft(i, 2);
+          count2++;
+        }
+      }
+    }
+    has5 = count5 >= 1 ? true : false;
+    has4 = count4 >= 2 ? true : false;
+    has3 = count3 >= 3 ? true : false;
+    has2 = count2 >= 4 ? true : false;
+    if (has2 && has3 && has4 && has5) {
+      countDone = true;
+    }
+  }
+}
+
+function PlacePiecesRight() {
+  let count5 = 0;
+  let count4 = 0;
+  let count3 = 0;
+  let count2 = 0;
+  let has5 = false;
+  let has4 = false;
+  let has3 = false;
+  let has2 = false;
+  var countDone = false;
+  while (!countDone) {
+    let i = getRandom(99);
+    if (!has5) {
+      if (i % 10 <= 5) {
+        if (
+          !isOccupiedRight(i, 5) &&
+          !isAdjacentCellOccupied(i, 3, rightCells)
+        ) {
+          PlaceRight(i, 5);
+          count5++;
+        }
+      }
+    }
+
+    if (!has4) {
+      if (i % 10 <= 6) {
+        if (
+          !isOccupiedRight(i, 4) &&
+          !isAdjacentCellOccupied(i, 3, rightCells)
+        ) {
+          PlaceRight(i, 4);
+          count4++;
+        }
+      }
+    }
+    if (!has3) {
+      if (i % 10 <= 7) {
+        if (
+          !isOccupiedRight(i, 3) &&
+          !isAdjacentCellOccupied(i, 3, rightCells)
+        ) {
+          PlaceRight(i, 3);
+          count3++;
+        }
+      }
+    }
+    if (!has2) {
+      if (i % 10 <= 8) {
+        if (
+          !isOccupiedRight(i, 2) &&
+          !isAdjacentCellOccupied(i, 3, rightCells)
+        ) {
+          PlaceRight(i, 2);
+          count2++;
+        }
+      }
+    }
+    has5 = count5 == 1 ? true : false;
+    has4 = count4 == 2 ? true : false;
+    has3 = count3 == 3 ? true : false;
+    has2 = count2 == 4 ? true : false;
+    if (has2 && has3 && has4 && has5) {
+      countDone = true;
+    }
+  }
+}
+
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+}
+function isOccupiedLeft(i, sLength) {
+  for (let o = 0; o < sLength; o++) {
+    let cell = leftCells[i + o];
+    if (cell.getAttribute("isOccupied") == "1") {
+      return true;
+    }
+  }
+  return false;
+}
+function isOccupiedRight(i, sLength) {
+  for (let o = 0; o < sLength; o++) {
+    let cell = rightCells[i + o];
+    if (cell.getAttribute("isOccupied") == "1") {
+      return true;
+    }
+  }
+  return false;
+}
+function PlaceLeft(i, sLength) {
+  if (!isAdjacentCellOccupied(i, sLength, leftCells)) {
+    for (let o = 0; o < sLength; o++) {
+      let cell = leftCells[i + o];
+      cell.setAttribute("isOccupied", "1");
+      cell.style.background = leftShipColor;
+    }
+  }
+}
+
+function PlaceRight(i, sLength) {
+  if (!isAdjacentCellOccupied(i, sLength, rightCells)) {
+    for (let o = 0; o < sLength; o++) {
+      let cell = rightCells[i + o];
+      cell.setAttribute("isOccupied", "1");
+      cell.style.background = leftShipColor;
+    }
+  }
+}
+
+function SwitchTurns() {
+  currentTurn = currentTurn == 0 ? 1 : 0;
+  if (currentTurn == 1) {
+    sleep(2000);
+    let random = getRandom(99);
+    ComputerAttack(random);
+  }
+}
+
+function Attacked(e) {
+  if (currentTurn == 0) {
+    let cnt = 0;
+    while (true) {
+      if (e.getAttribute("isHit") == 0) {
+        if (e.getAttribute("isOccupied") == 1) {
+          e.style.background = hitColor;
+          e.setAttribute("isHit", "1");
+        } else if (e.getAttribute("isOccupied") == 0) {
+          e.style.background = missColor;
+          e.setAttribute("isHit", "1");
+          break;
+        } else {
+          alert("WHAT ARE YOU THINKING COMMANDER?!\nWe already hit that spot.");
+        }
+      }
+      cnt++;
+      if (cnt > 1000) break;
+    }
+    SwitchTurns();
+  } else {
+    alert("Await your turn!");
+  }
+}
+
+function ComputerAttack(i) {
+  while (true) {
+    if (leftCells[i].getAttribute("isHit") == 0) {
+      if (leftCells[i].getAttribute("isOccupied") == 1) {
+        //computer hit
+        leftCells[i].style.background = hitColor;
+        leftCells[i].setAttribute("isHit", "1");
+      } else if (leftCells[i].getAttribute("isOccupied") == 0) {
+        leftCells[i].style.background = missColor;
+        leftCells[i].setAttribute("isHit", "1");
+        break;
+      }
+    }
+    i = getRandom(99);
+  }
+  SwitchTurns();
+}
+
+function isAdjacentCellOccupied(currentSquare, shipLength, board) {
+  currentSquare = parseInt(currentSquare);
+  // check left side
+  for (let i = 1; i < shipLength; i++) {
+    let cell = board[currentSquare - i];
+    if (!cell || cell.getAttribute("isOccupied") == "1") {
+      return true;
+    }
+  }
+
+  // check right side
+  for (let i = 1; i < shipLength; i++) {
+    let cell = board[currentSquare + i];
+    if (!cell || cell.getAttribute("isOccupied") == "1") {
+      return true;
+    }
+  }
+
+  // check top side
+  for (let i = 1; i < shipLength; i++) {
+    let cell = board[currentSquare - i * 10];
+    if (!cell || cell.getAttribute("isOccupied") == "1") {
+      return true;
+    }
+  }
+
+  // check bottom side
+  for (let i = 1; i < shipLength; i++) {
+    let cell = board[currentSquare + i * 10];
+    if (!cell || cell.getAttribute("isOccupied") == "1") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+//https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
